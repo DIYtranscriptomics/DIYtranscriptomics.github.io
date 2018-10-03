93e07cd635744260a24dfc8c139d288e36c59ba6
daniel beiting <danielbeiting@modv-ve503-1028.apn.wlan.upenn.edu>
2018-10-03 14:29:46 -0400

    updating step 2

--- Code/files/Step2_dataExploration.R
+++ Code/files/Step2_dataExploration.R
@@ -6,31 +6,32 @@
 # Load packages -----
 library(tidyverse) 
 library(RColorBrewer) # provides access to color palettes for graphics
-library(reshape2)
+library(reshape2) # for reshaping dataframes
 library(trelliscopejs) # must be downloaded from github
 library(genefilter)
-library(limma) # the main package we'll use for differential expression in R
 library(edgeR) # also for differential expression, but we only use for the DGEList object
-library(Biobase)
+library(matrixStats) # let's us easily calculate stats on any matrix rows or columns
 
 # Identify variables of interest in study design file ----
 targets
-groups <- targets$treatment2
-groups <- factor(groups)
+groups1 <- targets$treatment
+groups2 <- targets$treatment2
+groups1 <- factor(groups1)
+groups2 <- factor(groups2)
 sampleLabels <- targets$sample
 
 # Examine your data up to this point ----
-myTPM <- Txi_gene$abundance
+myCPM <- Txi_gene$abundance
 myCounts <- Txi_gene$counts
-# Do you think counts or TPM are appropriate for multivariate statistical analysis....why?
 # let's quickly graph both matrices to see what we're dealing with
 
 # Take a look at the heteroskedasticity of the data ----
-# first, calculate row means and standard deviations for each transcript or gene and add these to your data frame
-myTPM.stats <- transform(myTPM, 
-                         SD=rowSds(myTPM), 
-                         AVG=rowMeans(myTPM),
-                         MED=rowMedians(myTPM)
+# first, calculate row means and standard deviations for each transcript or gene 
+# and add these to your data matrix
+myCPM.stats <- transform(myCPM, 
+                         SD=rowSds(myCPM), 
+                         AVG=rowMeans(myCPM),
+                         MED=rowMedians(myCPM)
                          )
 
 myCounts.stats <- transform(myCounts, 
@@ -39,51 +40,83 @@ myCounts.stats <- transform(myCounts,
                             MED=rowMedians(myCounts)
                             )
 
-head(myTPM.stats)
+head(myCPM.stats)
 #produce a scatter plot of the transformed data
-ggplot(myTPM.stats, aes(x=SD, y=MED)) +
-  geom_point(shape=1) +
-  geom_point(size=4)
-# how might you expect that counts and TPM compare if used as input for PCA analysis?
+ggplot(myCPM.stats, aes(x=SD, y=MED)) +
+  geom_point(shape=1, size=4)
+# Experiment with point shape and size
+# experiment with geom_hex
 # how would these graphs change if you log2 converted the data?
 
 # Make a DGElist from your counts ----
 DGEList <- DGEList(Txi_gene$counts)
+# take a look at the DGEList object 
+DGEList
+#DEGList objects are a good R data file to consider saving to you working directory
+save(DGEList, file = "myDGEList")
+#Saved DGEList objects can be easily shared and loaded into an R environment
+load(file = "myDGEList")
+
 # use the 'cpm' function from EdgeR to get counts per million
 cpm <- cpm(DGEList) 
+colSums(cpm)
 log2.cpm <- cpm(DGEList, log=TRUE)
 
 # Take a look at the distribution of the Log2 CPM
 nsamples <- ncol(log2.cpm)
 myColors <- brewer.pal(nsamples, "Paired")
-boxplot(log2.cpm, las=2, cex=1, col = myColors, names = sampleLabels, main="non-normalized log2 cpm")
-#what do you see as some potential issues here?
+
+# 'coerce' your data matrix to a dataframe so that you can use tidyverse tools on it
+log2.cpm.df <- as.tibble(log2.cpm)
+log2.cpm.df
+# add your sample names to this dataframe (we lost these when we read our data in with tximport)
+colnames(log2.cpm.df) <- sampleLabels
+# use the reshape2 package to 'melt' your dataframe (from wide to tall)
+log2.cpm.df.melt <- melt(log2.cpm.df)
+Log2.cpm.df.melt <- as.tibble(log2.cpm.df.melt)
+Log2.cpm.df.melt
+
+ggplot(Log2.cpm.df.melt, aes(x=variable, y=value, fill=variable)) +
+  geom_violin(trim = TRUE, show.legend = TRUE) +
+  stat_summary(fun.y = "median", geom = "point", shape = 95, size = 10, color = "black") +
+  theme_bw()
+# what do you think of the distribution of this data?
 
 # Filter your data ----
 #first, take a look at how many genes or transcripts have no read counts at all
 table(rowSums(DGEList$counts==0)==9)
+
 # now set some cut-off to get rid of genes/transcripts with low counts
 keepers <- rowSums(cpm>1)>=3
 DGEList.filtered <- DGEList[keepers,]
 dim(DGEList.filtered)
 
 log2.cpm.filtered <- cpm(DGEList.filtered, log=TRUE)
-boxplot(log2.cpm.filtered, 
-        las=2, cex=1, 
-        col = myColors, 
-        names = sampleLabels, 
-        main="non-normalized log2 cpm")
+log2.cpm.filtered.df <- as.tibble(log2.cpm.filtered) 
+colnames(log2.cpm.filtered.df) <- sampleLabels
+log2.cpm.filtered.df.melt <- melt(log2.cpm.filtered.df)
+log2.cpm.filtered.df.melt <- as.tibble(log2.cpm.filtered.df.melt)
+
+ggplot(log2.cpm.filtered.df.melt, aes(x=variable, y=value, fill=variable)) +
+  geom_violin(trim = TRUE, show.legend = TRUE) +
+  stat_summary(fun.y = "median", geom = "point", shape = 95, size = 10, color = "black") +
+  theme_bw()
 
 # Normalize your data ----
 DGEList.filtered.norm <- calcNormFactors(DGEList.filtered, method = "TMM")
 
-# use the 'cpm' function from EdgeR to get counts per million from you 
+# use the 'cpm' function from EdgeR to get counts per million from your normalized data
 log2.cpm.filtered.norm <- cpm(DGEList.filtered.norm, log=TRUE)
-boxplot(log2.cpm.filtered.norm, 
-        las=2, cex=1, 
-        col = myColors, 
-        names = sampleLabels, 
-        main="normalized log2 cpm")
+
+log2.cpm.filtered.norm.df <- as.tibble(log2.cpm.filtered.norm) 
+colnames(log2.cpm.filtered.norm.df) <- sampleLabels
+log2.cpm.filtered.norm.df.melt <- melt(log2.cpm.filtered.norm.df)
+log2.cpm.filtered.norm.df.melt <- as.tibble(log2.cpm.filtered.norm.df.melt)
+
+ggplot(log2.cpm.filtered.norm.df.melt, aes(x=variable, y=value, fill=variable)) +
+  geom_violin(trim = TRUE, show.legend = TRUE) +
+  stat_summary(fun.y = "median", geom = "point", shape = 95, size = 10, color = "black") +
+  theme_bw()
 
 
 # Hierarchical clustering ---------------
@@ -96,45 +129,31 @@ plot(clusters, labels=sampleLabels)
 # Pricipal component analysis (PCA) -------------
 pca.res <- prcomp(t(log2.cpm.filtered.norm), scale.=F, retx=T)
 #look at pca.res in environment
-class(pca.res)
 ls(pca.res)
 summary(pca.res) # Prints variance summary for all principal components.
-head(pca.res$rotation) #$rotation shows you how much each TRANSCRIPT influenced each PC (called 'loadings')
-head(pca.res$x) #$x shows you how much each SAMPLE influenced each PC (called 'scores')
-plot(pca.res, las=1)
+pca.res$rotation #$rotation shows you how much each gene influenced each PC (called 'scores')
+pca.res$x #$x shows you how much each sample influenced each PC (called 'loadings')
+#note that these loadings have a magnitude and a direction (this is the basis for making a PCA plot)
 pc.var<-pca.res$sdev^2 #sdev^2 gives you the eigenvalues
 pc.per<-round(pc.var/sum(pc.var)*100, 1)
 pc.per
 
 # Visualize your PCA result ------------------
 #lets first plot any two PCs against each other
-#turn your scores for each gene into a data frame
-data.frame <- as.data.frame(pca.res$x)
-ggplot(data.frame, aes(x=PC1, y=PC2, color=groups)) +
+#We know how much each sample contributes to each PC (loadings), so let's plot
+pca.res.df <- as.tibble(pca.res$x)
+ggplot(pca.res.df, aes(x=PC1, y=PC2, color=groups2)) +
   geom_point(size=5) +
   theme(legend.position="right") 
+#what other variables could you 'paint' onto this PCA plot
+#how would this PCA look if you used raw counts (myCounts) instead of log2 CPM?
 
 # Create a PCA 'small multiples' chart ----
-# this is another way to view PCA to understand impact of each variable on each pricipal component
-melted <- cbind(groups, melt(pca.res$x[,1:4]))
+# this is another way to view PCA laodings to understand impact of each sample on each pricipal component
+melted <- cbind(groups2, melt(pca.res$x[,1:4]))
 head(melted)
 #look at your 'melted' data
 ggplot(melted) +
-  geom_bar(aes(x=Var1, y=value, fill=groups), stat="identity") +
+  geom_bar(aes(x=Var1, y=value, fill=groups2), stat="identity") +
   facet_wrap(~Var2)
   #facet_trelliscope(~Var2) #add this trelliscope layer to the ggplot to have some fun
-
-# OPTIONAL: Explore other plotting options on your own ----
-# T-sne, NMDS, reactive graphs, 3D interactives, etc
-library(scatterD3) #makes nice interactive 2D plots using the D3 engine
-library(rgl)
-library(pca3d) # good for 3D PCA plots
-library(RColorBrewer)
-test <- cbind(pca.res$x, c(1,1,1,2,2,2,3,3,3))
-colnames(test)[10] <- "colvector"
-scatterD3(test, 
-          x = PC1, y = PC2,
-          col_var = colvector)
-pca3d(pca.res$x[,1:3], 
-      radius = 3,
-      col = c("blue", "blue", "blue", "red", "red", "red", "green", "green", "green"))
