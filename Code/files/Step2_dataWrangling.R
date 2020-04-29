165045c23680a8f28228c9c3adfc4952fdcc4704
daniel beiting <danielbeiting@daniels-mbp-3.lan>
2020-04-29 14:18:04 -0400

    posting vids for lecture5 and updated step2 script

--- Code/files/Step2_dataWrangling.R
+++ Code/files/Step2_dataWrangling.R
@@ -1,12 +1,16 @@
-# Introduction to this script ----
-# now that you've read your transcript-level or gene-level data into R, you're ready to begin working with your data
+# Introduction to the Step 2 script ----
+# Now that you've read your transcript-level or gene-level data into R, you're ready to begin working with your data.
+
+# Step 2 Learning Objectives:
+# 1 - Filter and normalize your data
+# 2 - use ggplot2 to visualize the impact of filtering and normalization on our data.
+# 3 - understand why gene expression data is messy, and how to make it 'tidy'
+
+# Notes:
 # recall that your abundance data are TPM, while the counts are read counts mapping to each gene or transcript
-# Our goals in this script are to filter and normalize data
-# This script also introduces us to ggplot2 for plotting, which we'll use to visually depict the impact of filtering and normalization on our data.
 
 # Load packages -----
 library(tidyverse) # already know about this from Step 1 script
-library(reshape2) # for reshaping dataframes so they play nice with plotting functions
 library(genefilter) # as the package name suggests, it's for filtering genes
 library(edgeR) # well known package for differential expression analysis, but we only use for the DGEList object and for normalization methods
 library(matrixStats) # let's us easily calculate stats on rows or columns of a data matrix
@@ -15,7 +19,7 @@ library(cowplot) # allows you to combine multiple plots in one figure
 # Examine your data up to this point ----
 myTPM <- Txi_gene$abundance
 myCounts <- Txi_gene$counts
-colSums(myTPM)
+colSums(myTPM_trans)
 colSums(myCounts)
 
 # capture sample labels from the study design file that you worked with and saved as 'targets' in step 1
@@ -25,19 +29,38 @@ sampleLabels <- targets$sample
 # Generate summary stats for your data ----
 # 1st, calculate summary stats for each transcript or gene, and add these to your data matrix
 # then use the base R function 'transform' to modify the data matrix (equivalent of Excel's '=')
+# then we use the 'rowSds', 'rowMeans' and 'rowMedians' functions from the matrixStats package
 myTPM.stats <- transform(myTPM, 
                          SD=rowSds(myTPM), 
                          AVG=rowMeans(myTPM),
                          MED=rowMedians(myTPM))
 
-#look at what you created
+# look at what you created
 head(myTPM.stats)
-#produce a scatter plot of the transformed data
-ggplot(myTPM.stats, aes(x=SD, y=MED)) +
-  geom_hex(shape=16, size=2)
-# Experiment with point shape and size
+
+# Create your first plot using ggplot2 ----
+# produce a scatter plot of the transformed data
+ggplot(myTPM.stats) + 
+  aes(x = SD, y = MED) +
+  geom_point(shape=16, size=2)
+# Experiment with point shape and size in the plot above
 # Experiment with other plot types (e.g. 'geom_hex' instead of 'geom_point')
+# Add a theme to your ggplot code above.  Try 'theme_bw()'
 # How would these graphs change if you log2 converted the data?
+
+# Let's expand on the plot above a bit more and take a look at each 'layer' of the ggplot code
+ggplot(myTPM.stats) + 
+  aes(x = SD, y = MED) +
+  geom_point(shape=16, size=2) +
+  geom_smooth(method=lm) +
+  geom_hex(show.legend = FALSE) +
+  labs(y="Median", x = "Standard deviation",
+       title="Transcripts per million (TPM)",
+       subtitle="unfiltered, non-normalized data",
+       caption="DIYtranscriptomics - Spring 2020") +
+  theme_classic() +
+  theme_dark() + 
+  theme_bw()
 
 # Make a DGElist from your counts, and plot ----
 myDGEList <- DGEList(myCounts)
@@ -58,38 +81,48 @@ log2.cpm.df <- as_tibble(log2.cpm, rownames = "geneID")
 log2.cpm.df
 # add your sample names to this dataframe (we lost these when we read our data in with tximport)
 colnames(log2.cpm.df) <- c("geneID", sampleLabels)
-# use the reshape2 package to 'melt' your dataframe (from wide to tall)
-log2.cpm.df.melt <- melt(log2.cpm.df)
+# use the tidy package to 'pivot' your dataframe (from wide to long)
+log2.cpm.df.pivot <- pivot_longer(log2.cpm.df, # dataframe to be pivoted
+                                  cols = HS01:CL13, # column names to be stored as a SINGLE variable
+                                  names_to = "samples", # name of that new variable (column)
+                                  values_to = "expression") # name of new variable (column) storing all the values (data)
 
-ggplot(log2.cpm.df.melt, aes(x=variable, y=value, fill=variable)) +
+# let's look at the impact of pivoting the data
+log2.cpm.df.pivot
+
+# not it is easy to plot this pivoted data
+ggplot(log2.cpm.df.pivot) +
+  aes(x=samples, y=expression, fill=samples) +
   geom_violin(trim = FALSE, show.legend = FALSE) +
-  stat_summary(fun.y = "median", 
+  stat_summary(fun = "median", 
                geom = "point", 
-               shape = 124, 
-               size = 6, 
+               shape = 95, 
+               size = 10, 
                color = "black", 
                show.legend = FALSE) +
   labs(y="log2 expression", x = "sample",
        title="Log2 Counts per Million (CPM)",
        subtitle="unfiltered, non-normalized",
-       caption=paste0("produced on ", Sys.time())) #using the Sys.time function from base R to print date/time on graph
+       caption=paste0("produced on ", Sys.time())) +
+  theme_bw()
   
 # what do you think of the distribution of this data?
 # Try using coord_flip() at the end of the ggplot code
 
 # Filter your data ----
 #first, take a look at how many genes or transcripts have no read counts at all
-table(rowSums(myDGEList$counts==0)==9)
+table(rowSums(myDGEList$counts==0)==10)
 # breaking down the line above is a little tricky.  Let's try:
 # 1st - 'myDGEList$counts==0' returns a new 'logical matrix' where each observation (gene) is evaluated (TRUE/FALSE) for each variable (sample) as to whether it has zero counts
 # 2nd - passing this logical matrix to 'rowsums' allows you to sum the total number of times an observation was 'TRUE' across all samples
-# 3rd - adding the '==9' is a simple way of asking how many of the rowsums equaled 9. In other words, how many genes had 0 counts (TRUE) for all samples
+# 3rd - adding the '==10' is a simple way of asking how many of the rowsums equaled 10. In other words, how many genes had 0 counts (TRUE) for all samples in our dataset
 # 4th - passing all this to the 'table' function just provides a handy way to summarize the large logical produced in the previous step
 
 # now set some cut-off to get rid of genes/transcripts with low counts
 # again using rowSums to tally up the 'TRUE' results of a simple evaluation
 # how many genes had more than 1CPM (TRUE) in at least 3 samples
-keepers <- rowSums(cpm>1)>=3
+
+keepers <- rowSums(cpm>1)>=5 # This step is important! Set up this cutoff for the number of samples in the smallest group of comparison.
 # now use base R's simple subsetting method to filter your DGEList based on the logical produced above
 myDGEList.filtered <- myDGEList[keepers,]
 dim(myDGEList.filtered)
@@ -97,20 +130,27 @@ dim(myDGEList.filtered)
 log2.cpm.filtered <- cpm(myDGEList.filtered, log=TRUE)
 log2.cpm.filtered.df <- as_tibble(log2.cpm.filtered, rownames = "geneID")
 colnames(log2.cpm.filtered.df) <- c("geneID", sampleLabels)
-log2.cpm.filtered.df.melt <- melt(log2.cpm.filtered.df)
+# pivot this FILTERED data, just as you did earlier
+log2.cpm.filtered.df.pivot <- pivot_longer(log2.cpm.filtered.df, # dataframe to be pivoted
+                                           cols = HS01:CL13, # column names to be stored as a SINGLE variable
+                                           names_to = "samples", # name of that new variable (column)
+                                           values_to = "expression") # name of new variable (column) storing all the values (data)
 
-ggplot(log2.cpm.filtered.df.melt, aes(x=variable, y=value, fill=variable)) +
+
+ggplot(log2.cpm.filtered.df.pivot) +
+  aes(x=samples, y=expression, fill=samples) +
   geom_violin(trim = FALSE, show.legend = FALSE) +
-  stat_summary(fun.y = "median", 
+  stat_summary(fun = "median", 
                geom = "point", 
-               shape = 124, 
-               size = 6, 
+               shape = 95, 
+               size = 10, 
                color = "black", 
                show.legend = FALSE) +
   labs(y="log2 expression", x = "sample",
        title="Log2 Counts per Million (CPM)",
        subtitle="filtered, non-normalized",
-       caption=paste0("produced on ", Sys.time()))
+       caption=paste0("produced on ", Sys.time())) +
+  theme_bw()
 
 # Normalize your data ----
 myDGEList.filtered.norm <- calcNormFactors(myDGEList.filtered, method = "TMM")
@@ -120,30 +160,41 @@ myDGEList.filtered.norm <- calcNormFactors(myDGEList.filtered, method = "TMM")
 log2.cpm.filtered.norm <- cpm(myDGEList.filtered.norm, log=TRUE)
 log2.cpm.filtered.norm.df <- as_tibble(log2.cpm.filtered.norm, rownames = "geneID")
 colnames(log2.cpm.filtered.norm.df) <- c("geneID", sampleLabels)
-log2.cpm.filtered.norm.df.melt <- melt(log2.cpm.filtered.norm.df)
+# pivot this NORMALIZED data, just as you did earlier
+log2.cpm.filtered.norm.df.pivot <- pivot_longer(log2.cpm.filtered.norm.df, # dataframe to be pivoted
+                                                cols = HS01:CL13, # column names to be stored as a SINGLE variable
+                                                names_to = "samples", # name of that new variable (column)
+                                                values_to = "expression") # name of new variable (column) storing all the values (data)
 
-ggplot(log2.cpm.filtered.norm.df.melt, aes(x=variable, y=value, fill=variable)) +
+
+ggplot(log2.cpm.filtered.norm.df.pivot) +
+  aes(x=samples, y=expression, fill=samples) +
   geom_violin(trim = FALSE, show.legend = FALSE) +
-  stat_summary(fun.y = "median", 
+  stat_summary(fun = "median", 
                geom = "point", 
-               shape = 124, 
-               size = 6, 
+               shape = 95, 
+               size = 10, 
                color = "black", 
                show.legend = FALSE) +
   labs(y="log2 expression", x = "sample",
        title="Log2 Counts per Million (CPM)",
        subtitle="filtered, TMM normalized",
-       caption=paste0("produced on ", Sys.time()))
+       caption=paste0("produced on ", Sys.time())) +
+  theme_bw()
+
+
 
 # what if we wanted to put all three violin plots together?
 # go back and assign each plot to a variable (rather than printing to the plots viewer)
+# here we assigned the last 3 plots to p1, p2 and p3
 # we'll use the 'plot_grid' function from the cowplot package to put these together in a figure
 plot_grid(p1, p2, p3, labels = c('A', 'B', 'C'), label_size = 12)
+print("Step 2 complete!")
 
 # the essentials ----
-library(reshape2) 
+library(tidyverse)
 library(genefilter)
-library(edgeR) 
+library(edgeR)
 library(matrixStats)
 
 sampleLabels <- targets$sample
@@ -152,60 +203,76 @@ log2.cpm <- cpm(myDGEList, log=TRUE)
 
 log2.cpm.df <- as_tibble(log2.cpm, rownames = "geneID")
 colnames(log2.cpm.df) <- c("geneID", sampleLabels)
-log2.cpm.df.melt <- melt(log2.cpm.df)
+log2.cpm.df.pivot <- pivot_longer(log2.cpm.df, # dataframe to be pivoted
+                                  cols = HS01:CL13, # column names to be stored as a SINGLE variable
+                                  names_to = "samples", # name of that new variable (column)
+                                  values_to = "expression") # name of new variable (column) storing all the values (data)
 
-p1 <- ggplot(log2.cpm.df.melt, aes(x=variable, y=value, fill=variable)) +
+p1 <- ggplot(log2.cpm.df.pivot) +
+  aes(x=samples, y=expression, fill=samples) +
   geom_violin(trim = FALSE, show.legend = FALSE) +
-  stat_summary(fun.y = "median", 
+  stat_summary(fun = "median", 
                geom = "point", 
-               shape = 124, 
-               size = 6, 
+               shape = 95, 
+               size = 10, 
                color = "black", 
                show.legend = FALSE) +
   labs(y="log2 expression", x = "sample",
        title="Log2 Counts per Million (CPM)",
        subtitle="unfiltered, non-normalized",
-       caption=paste0("produced on ", Sys.time()))
+       caption=paste0("produced on ", Sys.time())) +
+  theme_bw()
 
 cpm <- cpm(myDGEList)
-keepers <- rowSums(cpm>1)>=3 #user defined
+keepers <- rowSums(cpm>1)>=5 #user defined
 myDGEList.filtered <- myDGEList[keepers,]
 
 log2.cpm.filtered <- cpm(myDGEList.filtered, log=TRUE)
-log2.cpm.filtered.df <- as_tibble(log2.cpm.filtered, rownames = "geneID") 
+log2.cpm.filtered.df <- as_tibble(log2.cpm.filtered, rownames = "geneID")
 colnames(log2.cpm.filtered.df) <- c("geneID", sampleLabels)
-log2.cpm.filtered.df.melt <- melt(log2.cpm.filtered.df)
+log2.cpm.filtered.df.pivot <- pivot_longer(log2.cpm.filtered.df, # dataframe to be pivoted
+                                           cols = HS01:CL13, # column names to be stored as a SINGLE variable
+                                           names_to = "samples", # name of that new variable (column)
+                                           values_to = "expression") # name of new variable (column) storing all the values (data)
 
-p2 <- ggplot(log2.cpm.filtered.df.melt, aes(x=variable, y=value, fill=variable)) +
+p2 <- ggplot(log2.cpm.filtered.df.pivot) +
+  aes(x=samples, y=expression, fill=samples) +
   geom_violin(trim = FALSE, show.legend = FALSE) +
-  stat_summary(fun.y = "median", 
+  stat_summary(fun = "median", 
                geom = "point", 
-               shape = 124, 
-               size = 6, 
+               shape = 95, 
+               size = 10, 
                color = "black", 
                show.legend = FALSE) +
   labs(y="log2 expression", x = "sample",
        title="Log2 Counts per Million (CPM)",
        subtitle="filtered, non-normalized",
-       caption=paste0("produced on ", Sys.time()))
+       caption=paste0("produced on ", Sys.time())) +
+  theme_bw()
 
 myDGEList.filtered.norm <- calcNormFactors(myDGEList.filtered, method = "TMM")
 log2.cpm.filtered.norm <- cpm(myDGEList.filtered.norm, log=TRUE)
 log2.cpm.filtered.norm.df <- as_tibble(log2.cpm.filtered.norm, rownames = "geneID")
 colnames(log2.cpm.filtered.norm.df) <- c("geneID", sampleLabels)
-log2.cpm.filtered.norm.df.melt <- melt(log2.cpm.filtered.norm.df)
+log2.cpm.filtered.norm.df.pivot <- pivot_longer(log2.cpm.filtered.norm.df, # dataframe to be pivoted
+                                                cols = HS01:CL13, # column names to be stored as a SINGLE variable
+                                                names_to = "samples", # name of that new variable (column)
+                                                values_to = "expression") # name of new variable (column) storing all the values (data)
 
-p3 <- ggplot(log2.cpm.filtered.norm.df.melt, aes(x=variable, y=value, fill=variable)) +
+p3 <- ggplot(log2.cpm.filtered.norm.df.pivot) +
+  aes(x=samples, y=expression, fill=samples) +
   geom_violin(trim = FALSE, show.legend = FALSE) +
-  stat_summary(fun.y = "median", 
+  stat_summary(fun = "median", 
                geom = "point", 
-               shape = 124, 
-               size = 6, 
+               shape = 95, 
+               size = 10, 
                color = "black", 
                show.legend = FALSE) +
   labs(y="log2 expression", x = "sample",
        title="Log2 Counts per Million (CPM)",
        subtitle="filtered, TMM normalized",
-       caption=paste0("produced on ", Sys.time()))
+       caption=paste0("produced on ", Sys.time())) +
+  theme_bw()
 
 plot_grid(p1, p2, p3, labels = c('A', 'B', 'C'), label_size = 12)
+
