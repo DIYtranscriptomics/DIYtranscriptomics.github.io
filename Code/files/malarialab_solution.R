12160e075ff36e13be6c00fd6250343c79244a29
Daniel Beiting <danielbeiting@gmail.com>
2023-03-22 19:09:43 -0400

    minor updates

--- Code/files/malarialab_solution.R
+++ Code/files/malarialab_solution.R
@@ -4,8 +4,8 @@ library(tidyverse) # provides access to Hadley Wickham's collection of R package
 library(tximport) # package for getting Kallisto results into R
 targets <- read_tsv("studydesign.txt")# read in your study design
 path <- file.path(targets$sample, "abundance.tsv") # set file paths to your mapped data
-Txi_gene <- tximport(path, 
-                     type = "kallisto", 
+Txi_gene <- tximport(path,
+                     type = "kallisto",
                      txOut = TRUE, #determines whether your data represented at transcript or gene level
                      countsFromAbundance = "lengthScaledTPM",
                      ignoreTxVersion = TRUE)
@@ -29,11 +29,11 @@ log2.cpm.df.pivot <- pivot_longer(log2.cpm.df, # dataframe to be pivoted
 p1 <- ggplot(log2.cpm.df.pivot) +
   aes(x=samples, y=expression, fill=samples) +
   geom_violin(trim = FALSE, show.legend = FALSE) +
-  stat_summary(fun = "median", 
-               geom = "point", 
-               shape = 95, 
-               size = 10, 
-               color = "black", 
+  stat_summary(fun = "median",
+               geom = "point",
+               shape = 95,
+               size = 10,
+               color = "black",
                show.legend = FALSE) +
   labs(y="log2 expression", x = "sample",
        title="Log2 Counts per Million (CPM)",
@@ -56,11 +56,11 @@ log2.cpm.filtered.df.pivot <- pivot_longer(log2.cpm.filtered.df, # dataframe to 
 p2 <- ggplot(log2.cpm.filtered.df.pivot) +
   aes(x=samples, y=expression, fill=samples) +
   geom_violin(trim = FALSE, show.legend = FALSE) +
-  stat_summary(fun = "median", 
-               geom = "point", 
-               shape = 95, 
-               size = 10, 
-               color = "black", 
+  stat_summary(fun = "median",
+               geom = "point",
+               shape = 95,
+               size = 10,
+               color = "black",
                show.legend = FALSE) +
   labs(y="log2 expression", x = "sample",
        title="Log2 Counts per Million (CPM)",
@@ -80,11 +80,11 @@ log2.cpm.filtered.norm.df.pivot <- pivot_longer(log2.cpm.filtered.norm.df, # dat
 p3 <- ggplot(log2.cpm.filtered.norm.df.pivot) +
   aes(x=samples, y=expression, fill=samples) +
   geom_violin(trim = FALSE, show.legend = FALSE) +
-  stat_summary(fun = "median", 
-               geom = "point", 
-               shape = 95, 
-               size = 10, 
-               color = "black", 
+  stat_summary(fun = "median",
+               geom = "point",
+               shape = 95,
+               size = 10,
+               color = "black",
                show.legend = FALSE) +
   labs(y="log2 expression", x = "sample",
        title="Log2 Counts per Million (CPM)",
@@ -95,7 +95,7 @@ p3 <- ggplot(log2.cpm.filtered.norm.df.pivot) +
 plot_grid(p1, p2, p3, labels = c('A', 'B', 'C'), label_size = 12)
 
 
-# PCA and summary tables (step 3 script) ---- 
+# PCA and summary tables (step 3 script) ----
 library(DT)
 library(gt)
 library(plotly)
@@ -106,11 +106,11 @@ targets$group <- c("timepoint_00hr",
                    "timepoint_00hr",
                    "timepoint_08hr",
                    "timepoint_08hr",
-                   "timepoint_16hr", 
                    "timepoint_16hr",
-                   "timepoint_24hr", 
+                   "timepoint_16hr",
                    "timepoint_24hr",
-                   "timepoint_32hr", 
+                   "timepoint_24hr",
+                   "timepoint_32hr",
                    "timepoint_32hr",
                    "timepoint_40hr",
                    "timepoint_40hr",
@@ -123,13 +123,13 @@ group <- factor(group)
 
 pca.res <- prcomp(t(log2.cpm.filtered.norm), scale.=F, retx=T)
 pc.var <- pca.res$sdev^2 # sdev^2 captures these eigenvalues from the PCA result
-pc.per <- round(pc.var/sum(pc.var)*100, 1) 
+pc.per <- round(pc.var/sum(pc.var)*100, 1)
 pca.res.df <- as_tibble(pca.res$x)
 pca.plot <- ggplot(pca.res.df) +
   aes(x=PC1, y=PC2, color = group) +
   geom_point(size=4) +
   stat_ellipse() +
-  xlab(paste0("PC1 (",pc.per[1],"%",")")) + 
+  xlab(paste0("PC1 (",pc.per[1],"%",")")) +
   ylab(paste0("PC2 (",pc.per[2],"%",")")) +
   labs(title="PCA plot",
        caption=paste0("produced on ", Sys.time())) +
@@ -139,27 +139,27 @@ pca.plot <- ggplot(pca.res.df) +
 ggplotly(pca.plot)
 
 mydata.df <- mutate(log2.cpm.filtered.norm.df,
-                    timepoint_0hr = (totalRNA_0hr_rep1 + totalRNA_0hr_rep2)/2, 
-                    timepoint_8hr = (totalRNA_8hr_rep1 + totalRNA_8hr_rep2)/2, 
-                    timepoint_16hr = (totalRNA_16hr_rep1 + totalRNA_16hr_rep2)/2, 
-                    timepoint_24hr = (totalRNA_24hr_rep1 + totalRNA_24hr_rep2)/2, 
-                    timepoint_32hr = (totalRNA_32hr_rep1 + totalRNA_32hr_rep2)/2, 
-                    timepoint_48hr = (totalRNA_48hr_rep1 + totalRNA_48hr_rep2)/2, 
+                    timepoint_0hr = (totalRNA_0hr_rep1 + totalRNA_0hr_rep2)/2,
+                    timepoint_8hr = (totalRNA_8hr_rep1 + totalRNA_8hr_rep2)/2,
+                    timepoint_16hr = (totalRNA_16hr_rep1 + totalRNA_16hr_rep2)/2,
+                    timepoint_24hr = (totalRNA_24hr_rep1 + totalRNA_24hr_rep2)/2,
+                    timepoint_32hr = (totalRNA_32hr_rep1 + totalRNA_32hr_rep2)/2,
+                    timepoint_48hr = (totalRNA_48hr_rep1 + totalRNA_48hr_rep2)/2,
                     #now make columns comparing each of the averages above that you're interested in
                     LogFC = (timepoint_32hr - timepoint_0hr)) %>% #note that this is the first time you've seen the 'pipe' operator
   mutate_if(is.numeric, round, 2)
 
-datatable(mydata.df[,c(1,16,20,22)], 
-          extensions = c('KeyTable', "FixedHeader"), 
+datatable(mydata.df[,c(1,16,20,22)],
+          extensions = c('KeyTable', "FixedHeader"),
           filter = 'top',
-          options = list(keys = TRUE, 
-                         searchHighlight = TRUE, 
-                         pageLength = 10, 
+          options = list(keys = TRUE,
+                         searchHighlight = TRUE,
+                         pageLength = 10,
                          lengthMenu = c("10", "25", "50", "100")))
 
 #sort the above table by LogFC. Top ranked genes should be PF3D7_0102200.1.  What does a quick lit/google search indicate about this gene?
 
-# Create a PCA 'small multiples' chart ----
+# Create a PCA 'small multiples' chart
 # this is another way to view PCA laodings to understand impact of each sample on each pricipal component
 pca.res.df <- pca.res$x[,1:4] %>% # note that this is the first time you've seen the 'pipe' operator from the magrittr package
   as_tibble() %>%
@@ -200,7 +200,7 @@ contrast.matrix <- makeContrasts(DEGs_08hr = timepoint_08hr - timepoint_00hr,
                                  DEGs_48hr = timepoint_48hr - timepoint_00hr,
                                  levels=design)
 
-# extract the linear model fit 
+# extract the linear model fit
 fits <- contrasts.fit(fit, contrast.matrix)
 #get bayesian stats for your linear model fit
 ebFit <- eBayes(fits)
@@ -256,8 +256,8 @@ dim(diffGenes)
 diffGenes.df <- as_tibble(diffGenes, rownames = "geneID")
 
 # create interactive tables to display your DEGs
-datatable(diffGenes.df, 
-          extensions = c('KeyTable', "FixedHeader"), 
+datatable(diffGenes.df,
+          extensions = c('KeyTable', "FixedHeader"),
           caption = 'Table 1: DEGs in cutaneous leishmaniasis',
           options = list(keys = TRUE, searchHighlight = TRUE, pageLength = 10, lengthMenu = c("10", "25", "50", "100"))) %>%
   formatRound(columns=c(2:11), digits=2)
@@ -272,18 +272,18 @@ myheatcolors <- rev(brewer.pal(name="RdBu", n=11))
 clustRows <- hclust(as.dist(1-cor(t(diffGenes), method="pearson")), method="complete") #cluster rows by pearson correlation
 clustColumns <- hclust(as.dist(1-cor(diffGenes, method="spearman")), method="complete")
 module.assign <- cutree(clustRows, k=2)
-module.color <- rainbow(length(unique(module.assign)), start=0.1, end=0.9) 
-module.color <- module.color[as.vector(module.assign)] 
-heatmap.2(diffGenes, 
-          Rowv=as.dendrogram(clustRows), 
+module.color <- rainbow(length(unique(module.assign)), start=0.1, end=0.9)
+module.color <- module.color[as.vector(module.assign)]
+heatmap.2(diffGenes,
+          Rowv=as.dendrogram(clustRows),
           Colv=as.dendrogram(clustColumns),
           RowSideColors=module.color,
           col=myheatcolors, scale='row', labRow=NA,
-          density.info="none", trace="none",  
+          density.info="none", trace="none",
           cexRow=1, cexCol=1, margins=c(8,20))
 
 # this heatmap is a good example of when clustering columns might be problematic.  Repeat heatmap without clustering columns.  Which do you prefer and why?
-# Just-in-time gene expression: 
+# Just-in-time gene expression:
 # originally described here: https://journals.plos.org/plosbiology/article?id=10.1371/journal.pbio.0000005
 # more recent analysis of this phenomenon here: https://www.science.org/doi/10.1126/science.aba4357
 
