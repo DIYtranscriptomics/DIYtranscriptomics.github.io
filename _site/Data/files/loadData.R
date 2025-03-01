9713d6fb92c20a4cf52332ef447c08d0fc829074
Daniel Beiting <danielbeiting@gmail.com>
2025-02-28 23:31:52 -0500

    tweaks to week 7 material

--- _site/Data/files/loadData.R
+++ _site/Data/files/loadData.R
@@ -7,9 +7,9 @@ library(tidyverse)
 targets <- read_tsv("covid_metadata.txt")
 # then read in the human covid data and convert to a matrix with gene symbols as rownames
 human_covid_data <- read_tsv("GSE147507_RawReadCounts_Human.tsv")
-human_covid_data <- as.matrix(column_to_rownames(human_covid_data, "X1"))
+human_covid_data <- as.matrix(column_to_rownames(human_covid_data, "...1"))
 # repeat for the ferret covid data
 ferret_covid_data <- read_tsv("GSE147507_RawReadCounts_Ferret.tsv")
-ferret_covid_data <- as.matrix(column_to_rownames(ferret_covid_data, "X1"))
+ferret_covid_data <- as.matrix(column_to_rownames(ferret_covid_data, "...1"))
 
 # Now proceed with your exploration and analysis of the data!
